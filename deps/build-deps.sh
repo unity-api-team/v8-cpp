c147248bd44c61e25592a2ed8752474962f5201a
Marcus Tomlinson <themarcustomlinson@gmail.com>
2018-10-16 18:35:26 +0100

    gclient config

--- deps/build-deps.sh
+++ deps/build-deps.sh
@@ -6,6 +6,18 @@ export PATH=$DIR/depot_tools:"$PATH"
 
 cd $DIR/v8
 
+gclient config --spec 'solutions = [
+  { "name"        : "v8",
+    "url"         : "https://chromium.googlesource.com/v8/v8.git",
+    "deps_file"   : "DEPS",
+    "managed"     : False,
+    "custom_deps" : {
+    },
+    "safesync_url": "",
+  },
+]
+cache_dir = None'
+
 gclient sync
 
 ../gm.py x64.release
